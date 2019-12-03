2e07c9c14ed8768a98adf2aa047fe8d031bd93ad
elitonlunardi <lunardi.eliton@gmail.com>
2019-12-02 21:57:33 -0300

    adicionado connString no appsettings

--- src/PagueVeloz.Teste.Infra.Data/Context/PagueVelozContext.cs
+++ src/PagueVeloz.Teste.Infra.Data/Context/PagueVelozContext.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PagueVeloz.Teste.Infra.Data.Mappings;
 
 namespace PagueVeloz.Teste.Infra.Data
 {
     public class PagueVelozContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public PagueVelozContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer
-            (@"Server=localhost;Database=PagueVelozTeste;Integrated Security=True");
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("pagueVeloz"));
             base.OnConfiguring(optionsBuilder);
         }
 
